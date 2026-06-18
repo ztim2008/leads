0a473c365eecdc4a0f852378e3ecce5f02c433cd
root <root@fiwksyqpwx.local>
2026-06-18 15:36:36 +0000

    feat: Яндекс Метрика на Leads AI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
@@ -45,6 +46,10 @@ export default function RootLayout({
         style={{ fontFamily: "var(--font-sans)" }}
       >
         {children}
+      
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(98765432,"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});`}
+        </Script>
       </body>
     </html>
   );
